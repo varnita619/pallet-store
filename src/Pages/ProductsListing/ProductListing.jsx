a202f81e840c0cd8418d177df9d3a49dc6d999cb
Varnita Makrariya <crabthe64@gmail.com>
2022-03-18 23:49:10 +0530

    refactor: changed wishlist button

--- src/Pages/ProductsListing/ProductListing.jsx
+++ src/Pages/ProductsListing/ProductListing.jsx
@@ -1,48 +1,47 @@
 import { useState, useEffect } from "react";
-import "../ProductsListing/ProductListing.css"
-import { Link } from 'react-router-dom';
-import "../../CSS/Reset.css"
-import axios from "axios"
-import { SideBar } from "../../Components/SideBar/SideBar"
-
-
+import "../ProductsListing/ProductListing.css";
+import { Link } from "react-router-dom";
+import "../../CSS/Reset.css";
+import axios from "axios";
+import { SideBar } from "../../Components/SideBar/SideBar";
 
 function ProductListing() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    async function getProducts() {
+    (async function () {
       try {
-        const { data: { products } } = await axios.get('/api/products');
+        const {
+          data: { products },
+        } = await axios.get("/api/products");
         console.log(products);
 
-        setData(products)
-
+        setData(products);
       } catch (error) {
         console.error(error);
       }
-    }
-    getProducts();
-
-  }, [])
-  console.log(data)
+    })()
+  
+  }, []);
+  console.log(data);
 
   return (
     <div>
-
       <div className="main-container">
-
         <SideBar />
 
         <main className="products-wrapper">
           <h2 className="products-heading">All Products</h2>
 
-
           {data.map(({ id, name, company, price, rating, imageURL }) => {
             return (
               <div key={id}>
                 <div className="card">
-                  <Link to='/wishlist'><span className="card-badge"><i className="far fa-heart"></i></span></Link>
+                  <Link to="/wishlist">
+                    <span className="card-badge">
+                      <i className="far fa-heart"></i>
+                    </span>
+                  </Link>
                   <div className="card-image">
                     <img src={imageURL} alt="card-image" />
                   </div>
@@ -57,19 +56,18 @@ function ProductListing() {
                   </div>
 
                   <div className="card-buttons">
-                    <Link to='/cart' className="btn1 cart-btn">
+                    <Link to="/cart" className="btn1 cart-btn">
                       <i className="fas fa-shopping-cart"></i> Add to cart
                     </Link>
                   </div>
                 </div>
               </div>
-            )
+            );
           })}
-
         </main>
       </div>
     </div>
-  )
+  );
 }
 
 export { ProductListing };
