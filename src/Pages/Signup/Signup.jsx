7370120ff69aff872355599b23973a26cdc89375
Varnita Makrariya <crabthe64@gmail.com>
2022-03-19 12:08:33 +0530

    refactor: removed inline styliing

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -30,7 +30,7 @@ function Signup() {
                             type="checkbox"
                             checked="checked"
                             name="remember"
-                            style={{marginBottom: '15px'}}
+                            className='remember-me-checkbox'
                         />
                         Remember me
                     </label>
